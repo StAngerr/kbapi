15e771b2db7c545108efae3ba951ffd1c633257b
tstekhnovych <taras.stekhnovych@intellias.com>
2022-01-10 00:09:25 +0200

    Added basic db

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,24 @@
 import express from 'express';
 import bodyParser from 'body-parser'
+import db from './utils/db'
+
 
 import skillRouter from "./routes/Skill.router";
 
 const app = express();
 const port = 3000;
 
-
 app.use(bodyParser.urlencoded({extended: false}))
 
 app.use('/skills', skillRouter);
 
 app.get( "/", ( req, res ) => {
-    const a = 10;
-    const b = a +13;
-    res.send( "Hello world!1" );
+    // test code
+    db.execute('SELECT * FROM test_tb').then((dbres) =>  {
+        console.log(dbres)
+        res.send(dbres[0])
+    })
+
 } );
 
 app.listen(port, () => console.log('Server started at http://localhost:' + port))
