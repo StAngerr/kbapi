d4d4bdc847ac1eab17106915b3710caa1f7315b9
tstekhnovych <taras.stekhnovych@intellias.com>
2022-05-20 22:25:08 +0300

    Mail outh data changed to env variables

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -5,11 +5,9 @@ import { EMAIL_DOMAIN } from "../constants/emails";
 const OAuth2 = google.auth.OAuth2;
 
 //TODO:  Move to process envs
-const clientId =
-  "950613768444-mgqp94sdj3sfebacqu0rldlrnf4pv2kr.apps.googleusercontent.com";
-const clientSecret = "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";
-const refreshToken =
-  "1//04JY_w6x275SUCgYIARAAGAQSNgF-L9IrZdShuisC2rza5Zk7IzYoFDPDihjPJ5Dr7ZA2oVioI2iW1I2glIb_vsmXbo3ODKip9g";
+const clientId = process.env.mailClientId;
+const clientSecret = process.env.mailClientSecret;
+const refreshToken = process.env.mailRefreshToken;
 
 const oAuthClient = new OAuth2(clientId, clientSecret);
 
